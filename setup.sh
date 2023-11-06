5c187da5f2eb3b4f2ad9aab595104d967d5afb79
moriyku <5351183+moriyku@users.noreply.github.com>
2023-11-06 18:02:05 +0900

    Fix symbolic link path and add checks for sudo and git commands.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 set -eu
-
 CURRENT_DIR=$(cd $(dirname $0); pwd)
+DOT_DIR=$CURRENT_DIR/home
 
-cd $CURRENT_DIR
+has() {
+  type "$1" > /dev/null 2>&1
+}
 
-echo "setup..."
-for f in dotfiles/.??*
+if ! has sudo; then
+    echo "Install sudo..."
+    apt install -y sudo
+fi
+
+if ! has git; then
+    echo "Install git..."
+    sudo apt install -y git
+fi
+
+cd $DOT_DIR
+
+echo "Setup..."
+for f in .??*
 do
-    ln -snfv "$f" $HOME/
+    ln -snfv "$DOT_DIR/$f" "$HOME/$f"
 done
+tput setaf 2 && echo "✔ Dotfiles installed successfully!" && tput sgr0
