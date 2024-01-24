c8f1f3d8397920fcf411cb097240f3d749df6142
moriyku <5351183+moriyku@users.noreply.github.com>
2024-01-24 17:10:44 +0900

    Add support for Windows

--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 
 ## Setup
 
-Download
+### Linux
 
 ```
 cd ~
 git clone https://github.com/moriyku/dotfiles.git
-```
 
-Install
-
-```
 chmod +x dotfiles/setup.sh
 dotfiles/setup.sh
+```
+
+### Windows
+
+- **Open PowerShell as Administrator**
+
+```
+cd ~
+git clone https://github.com/moriyku/dotfiles.git
+
+dotfiles\setup.ps1
 ```
---
+++ setup.ps1
@@ -0,0 +1,43 @@
+$ErrorActionPreference = "Stop"
+
+$scriptPath = (Get-Item -Path $MyInvocation.MyCommand.Path).Directory.FullName
+$homeDir = Join-Path $scriptPath "home"
+$profileDir = Join-Path $scriptPath "winprofile"
+
+# Test if a command exists
+function Has {
+    param([string]$Command)
+    try {
+        $null = Get-Command -Name $Command -ErrorAction Stop
+        return $true
+    } catch {
+        return $false
+    }
+}
+
+# Chocolatey
+# https://chocolatey.org/install
+if (-not (Has "choco")) {
+    Write-Host "Installing Chocolatey..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+# git
+if (-not (Has "git")) {
+    Write-Host "Install git..."
+    choco install -y git.install
+}
+
+# .gitconfig
+Copy-Item -Path (Join-Path $profileDir ".gitconfig") -Destination (Join-Path $HOME ".gitconfig") -Force
+
+# .commit_template
+Copy-Item -Path (Join-Path $homeDir ".commit_template") -Destination (Join-Path $HOME ".commit_template") -Force
+
+# vim
+if (-not (Has "vim")) {
+    Write-Host "Install vim..."
+    choco install -y vim
+}
+
+Write-Host "Dotfiles installed successfully!" -ForegroundColor Green
